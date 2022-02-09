3db91a0d1ab8dc4ec6fe6f25aa7a25927cbd0ce0
Kewyn1488 <kirtsikhanau@gmail.com>
2022-02-09 17:31:29 +0300

    top header ( main nav panel) full ready , half of main header is ready

--- src/components/main/header.tsx
+++ src/components/main/header.tsx
@@ -1,68 +1,236 @@
 import React from 'react';
-import styled,{css} from "styled-components";
+import styled, {css} from "styled-components";
 
 const Header = () => {
 
-    const fontNavStyles = css `
-      font-family: Museo Sans Cyrl,sans-serif;
+    const fontNavStyles = css`
+      font-family: Museo Sans Cyrl, sans-serif;
       font-size: 16px;
       font-style: normal;
-      font-weight: 300;
       line-height: 18px;
-      letter-spacing: 0;
-      //text-align: right;
+      letter-spacing: .3px;
+      color: #FFFFFF;
     `
 
-    const Header = styled.header `
-        
+    const Header = styled.header`
+      flex-wrap: wrap;
     `
 
-    const TopHeader = styled.div `
-      width: 1920px;
+    const TopHeader = styled.div`
+      width: 100%;
+      max-width: 1920px;
       height: 50px;
+      background: #333333;
+    `
+
+    const NavTopHeader = styled.div`
+      display: flex;
+      flex-direction: row;
+      height: 50px;
+      margin: 0 160px;
+      @media (max-width: 1500px) {
+        margin: 0 2%;
+      }
+      align-items: center;
+      justify-content: flex-start;
+    `
+
+    const NavText = styled.li`
+      ${fontNavStyles};
+      margin-right: 6%;
+
+      &:nth-child(4) {
+        //margin-right: 5%;
+      }
+
+      &:nth-child(5) {
+        //margin-left: 2%;
+        //margin-right: 5%;
+      }
+
+      &:nth-child(6) {
+        margin-right: 15%;
+      }
+
+      &:nth-child(7) {
+        margin-right: 0;
+        padding-right: 1%;
+        border-right: 1px solid #956D84;
+      }
+
+      &:nth-child(8) {
+        margin-right: 0;
+        
+        padding-left: 1%;
+        //margin-left: auto;
+        
+      }
+
+      display: flex;
+    `
+
+    const NavImages = styled.img`
+      margin-right: 10px;
+    `
+
+    const DropDownItem = styled.span`
+      padding-left: 10%;
+      padding-right: 5%;
+      justify-content: space-between;
+      color: #956D84;
+    `
+
+    const ArrowDown = styled.img`
+      position: relative;
+      align-self: center;
+      width: 7px;
+      height: 4px;
+    `
+
+    const MainHeader = styled.div`
+      display: flex;
+      //flex-direction: row;
+      height: 200px;
+      margin: 0 160px;
+      @media (max-width: 1500px) {
+        margin: 0 2%;
+      }
+      //align-items: center;
+      //justify-content: flex-start;
+    `
+
+    const LogoImage = styled.img`
+      align-self: center;
+      margin-right: 8%;
+      width: 120px;
+      height: 120px;
+
+    `
+
+    const NavMainHeader = styled.div`
+
+    `
+
+    const SearchGroup = styled.div`
+      width: 100%;
+      margin-top: 4%;
+      justify-content: flex-start;
+    `
+
+
+
+    const CartInfoIcons = styled.div`
+
+    `
+
+    const InputCategories = styled.input`
+
+      height: 18px;
+      width: 220px;
+      
+      font-family: Museo Sans Cyrl, sans-serif;
+      font-size: 14px;
+      font-style: normal;
+      line-height: 18px;
+      font-weight: 300;
+      color: #828282;
+      border: none;
+      border-bottom: 1px solid #956D84;
+      
+      
+      
+      ::placeholder {
+        color: #828282;
+      }
+    `
+
+    const InputSearch = styled.input`
+
+      height: 18px;
+      width: 580px;
+      
+      font-family: Museo Sans Cyrl, sans-serif;
+      font-size: 14px;
+      font-style: normal;
+      line-height: 18px;
+      color: #828282;
+      border: none;
+      border-bottom: 1px solid #956D84;
     
-      background:#333333;
+      margin-right: 1%; 
+        
+      ::placeholder {
+        color: #828282;
+      }
     `
-    const NavText = styled.li `
-      ${fontNavStyles}
+    const SocialGroupIcons = styled.div`
+      width: 100%;  
     `
 
-
+    const SocialIcon = styled.img `
+      
+      width: 30px;
+      height: 30px;
+      background: #956D84;
+      border-radius: 50%;
+    `
 
     return (
         <Header>
             <TopHeader>
-                <ul>
+                <NavTopHeader>
                     <NavText>Валюта
-                        <span>Грн</span>
-                        <img src={'/images/arrow-down.png'} alt='arrow-down'/>
+                        <DropDownItem>Грн</DropDownItem>
+                        <ArrowDown src={'/images/arrow-down.png'} alt='arrow-down'/>
                     </NavText>
                     <NavText>Язык
-                        <span>RU</span>
-                        <img src={'/images/arrow-down.png'} alt='arrow-down'/>
+                        <DropDownItem>RU</DropDownItem>
+                        <ArrowDown src={'/images/arrow-down.png'} alt='arrow-down'/>
                     </NavText>
                     <NavText>Город
-                        <span>Киев</span>
-                        <img src={'/images/arrow-down.png'} alt='arrow-down'/>
+                        <DropDownItem>Киев</DropDownItem>
+                        <ArrowDown src={'/images/arrow-down.png'} alt='arrow-down'/>
                     </NavText>
                     <NavText>
-                        <img src={'/images/like.svg'} alt='like'/>
+                        <NavImages src={'/images/like.svg'} alt='like'/>
                         Закладки
                     </NavText>
                     <NavText>
-                        <img src={'/images/shipping.svg'} alt='shipping'/>
+                        <NavImages src={'/images/shipping.svg'} alt='shipping'/>
                         Доставка и оплата
                     </NavText>
-                    <NavText>Контакты</NavText>
-
-                </ul>
-                <ul>
-                    <img src={'/images/user-logo.svg'} alt='user-logo'/>
-                    <NavText>Вход</NavText>
+                    <NavText>
+                        <NavImages src={'/images/contacts.svg'} alt='contacts'/>
+                        Контакты
+                    </NavText>
+                    <NavText>
+                        <NavImages src={'/images/user-logo.svg'} alt='user-logo'/>
+                        Вход
+                    </NavText>
                     <NavText>Регистрация</NavText>
-                </ul>
+                </NavTopHeader>
             </TopHeader>
-            <img src={'/images/logo.png'} alt='logo'/>
+            <MainHeader>
+                <LogoImage src={'/images/logo.png'} alt='logo'/>
+                <SearchGroup>
+                    <InputCategories type='input' placeholder='Поиск по категориям'/>
+                    <InputSearch type='input' placeholder='Поиск по товарам'/>
+                </SearchGroup>
+                <NavMainHeader>
+
+                </NavMainHeader>
+
+                <SocialGroupIcons>
+                    <SocialIcon src={'/images/viber.svg'} alt='viber-logo'/>
+                    <SocialIcon src={'/images/instagram.svg'} alt='inst-logo'/>
+                    <SocialIcon src={'/images/telegram.svg'} alt='tg-logo'/>
+                    <SocialIcon src={'/images/facebook.svg'} alt='fb-logo'/>
+                </SocialGroupIcons>
+
+                <CartInfoIcons>
+
+                </CartInfoIcons>
+            </MainHeader>
         </Header>
     );
 };
--- src/global.ts
+++ src/global.ts
@@ -11,11 +11,25 @@ export default createGlobalStyle`
   }
 
   body {
-    margin: 0;
+    width: 100%;
+    color: #ffffff;
+    max-width: 1920px;
+    
+    margin: 0 auto;
     padding: 0;
   }
   
   ul,li {
     list-style: none;
+  }
+
+  @font-face {
+    font-family: "MuseoSansCyrl100";
+    src: url("/fonts/MuseoSansCyrl-100.eot");
+    src: url("/fonts/MuseoSansCyrl-100.eot?#iefix")format("embedded-opentype"),
+    url("/fonts/MuseoSansCyrl-100.woff") format("woff"),
+    url("/fonts/MuseoSansCyrl-100.ttf") format("truetype");
+    font-style: normal;
+    font-weight: 300;
   }
   `
